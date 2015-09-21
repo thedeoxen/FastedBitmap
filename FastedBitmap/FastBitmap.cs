d2d33930eb97b884d4c0e53025394892c48592ff
Bakhvalov Pavel <thedeoxen@gmail.com>
2015-09-21 22:46:55 +0500

    complete setPixel, toBitmap

--- FastedBitmap/FastBitmap.cs
+++ FastedBitmap/FastBitmap.cs
@@ -12,11 +12,8 @@ namespace FastedBimap
     {
         public int Height { get; set; }
         public int Width { get; set; }
-
         public Bitmap imgBmp;
-
-        private Color[,] pixelsArray;
-
+        private ColorInMemory[,] pixelsArray;
 
 
         public FastBitmap(Bitmap img)
@@ -39,23 +36,18 @@ namespace FastedBimap
 
         }
 
-        /// <summary>
-        /// fix images bytes in map and create color array
-        /// </summary>
-        /// <param name="img"></param>
-        /// <returns>2d color array</returns>
-        private Color[,] getPixelArrayFromBmp(Bitmap img)
+        private ColorInMemory[,] getPixelArrayFromBmp(Bitmap img)
         {
-            Color[,] colorArray = new Color[Height, Width];
+            ColorInMemory[,] colorArray = new ColorInMemory[Height, Width];
 
             //Lock the bitmap's bits
             BitmapData imgData = img.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadOnly, img.PixelFormat);
             //Get the address in memory first bit
             IntPtr ptr = imgData.Scan0;
-            int bytes = Math.Abs(imgData.Stride) * Height;
-            byte[] rgbValues = new byte[bytes];
+            int bytesLenght = Math.Abs(imgData.Stride) * Height;
+            byte[] rgbValues = new byte[bytesLenght];
             //fill bytes array
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytesLenght);
             /*
                because stride rounds to 4 bit
             */
@@ -67,11 +59,11 @@ namespace FastedBimap
                 correctStride = imgData.Stride - Width * 3;
             }
             int x = 0, y = 0;
-            for (int i = 0; i < bytes - 2; i += 3)
+            for (int i = 0; i < bytesLenght - 2; i += 3)
             {
 
-                Color color = Color.FromArgb(255, rgbValues[i], rgbValues[i + 1], rgbValues[i + 2]);
-                colorArray[y, x] = color;
+                Color color = Color.FromArgb(255, rgbValues[i + 2], rgbValues[i + 1], rgbValues[i]);
+                colorArray[y, x] = new ColorInMemory(color, byteCount);
                 byteCount += 3;
                 x++;
                 if (byteCount == byteStringLenght)
@@ -82,18 +74,52 @@ namespace FastedBimap
                     byteCount = 0;
                 }
             }
+            img.UnlockBits(imgData);
             return colorArray;
-
         }
 
         public void SetPixel(int x, int y, Color color)
         {
-            throw new NotImplementedException();
+            //Lock the bitmap's bits
+            BitmapData imgData = imgBmp.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadOnly, imgBmp.PixelFormat);
+            //Get the address in memory first bit
+            IntPtr ptr = imgData.Scan0;
+            int bytesLenght = Math.Abs(imgData.Stride) * Height;
+            byte[] rgbValues = new byte[3];
+            
+            int byteCount = pixelsArray[y, x].positionInMemory;
+            rgbValues[0] = color.B;
+            rgbValues[1] = color.G;
+            rgbValues[2] = color.R;
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr+byteCount, 3);
+
+            imgBmp.UnlockBits(imgData);
+            pixelsArray[y, x].color = color;
+
         }
 
         public Color GetPixel(int x, int y)
         {
-            return pixelsArray[y, x];
+            return pixelsArray[y, x].color;
+        }
+
+        public Bitmap ToBitmap()
+        {
+            return imgBmp;
+        }
+
+        /// <summary>
+        /// class for save color and position in byte
+        /// </summary>
+        private class ColorInMemory
+        {
+            public Color color;
+            public int positionInMemory;
+            public ColorInMemory(Color color, int position)
+            {
+                this.color = color;
+                this.positionInMemory = position;
+            }
         }
     }
 }
--- speedTestOfBitmap/UnitTest1.cs
+++ speedTestOfBitmap/UnitTest1.cs
@@ -23,7 +23,6 @@ namespace speedTestOfBitmap
             }
         }
 
-
         [TestMethod]
         public void fastBitmapReadTest()
         {
@@ -64,6 +63,84 @@ namespace speedTestOfBitmap
                     img.SetPixel(x, y, Color.Red);
                 }
             }
+            Assert.IsTrue(img.GetPixel(0, 0).R == 255);
+            Assert.IsTrue(img.GetPixel(0, 0).G == 0);
+            Assert.IsTrue(img.GetPixel(0, 0).B == 0);
         }
+
+        [TestMethod]
+        public void originBitmapReadTestBigImg()
+        {
+            int redChanelSumm = 0;
+
+            Bitmap img = new Bitmap("../../image/2.jpg");
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    redChanelSumm += img.GetPixel(x, y).R;
+                }
+            }
+        }
+
+
+        [TestMethod]
+        public void fastBitmapReadTestBigImg()
+        {
+            int redChanelSumm = 0;
+
+            FastBitmap img = new FastBitmap("../../image/2.jpg");
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    redChanelSumm += img.GetPixel(x, y).R;
+                }
+            }
+        }
+
+
+        [TestMethod]
+        public void originBitmapWriteTestBigImg()
+        {
+            Bitmap img = new Bitmap("../../image/2.jpg");
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    img.SetPixel(x, y, Color.Red);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void fastBitmapWriteTestBigImg()
+        {
+            FastBitmap img = new FastBitmap("../../image/2.jpg");
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    img.SetPixel(x, y, Color.Red);
+                }
+            }
+            Assert.IsTrue(img.GetPixel(0, 0).R == 255);
+            Assert.IsTrue(img.GetPixel(0, 0).G == 0);
+            Assert.IsTrue(img.GetPixel(0, 0).B == 0);            
+        }
+
+        [TestMethod]
+        public void testConvertFastBitmapToBitmap()
+        {
+            FastBitmap img = new FastBitmap("../../image/1.jpg");
+            img.SetPixel(0, 0, Color.Red);
+            Bitmap bmp = img.ToBitmap();
+            Assert.IsInstanceOfType(bmp, typeof(Bitmap));
+            Assert.IsTrue(bmp.GetPixel(0, 0).R == 255);
+            Assert.IsTrue(bmp.GetPixel(0, 0).G == 0);
+            Assert.IsTrue(bmp.GetPixel(0, 0).B == 0);
+        }
+
+
     }
 }
